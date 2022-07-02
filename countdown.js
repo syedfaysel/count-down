177702ce85a7c69a18d292788d87ab8b1deb97be
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-07-02 12:20:00 +0600

    countdown 5o days

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('June 28,2022 00:00:00').getTime();
+    const countDate = new Date('Aug 22,2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
@@ -10,13 +10,22 @@ const countdown = () => {
     const hour = minute*60;
     const day = hour*24;
     
+
+    let textDay = 0;
+    var textHour = 0;
+    let textMin = 0;
+    let textSec = 0;
+   
     //calculate the time 
-    const textDay = Math.floor(gap/day);
-    const textHour = Math.floor(gap%day/hour);
-    const textMin = Math.floor(gap%hour/minute);
-    const textSec = Math.floor(gap%minute/second);
-    console.log(countDate);
-    console.log(textDay,textHour,textMin,textSec);
+    if(gap>=0){
+        textDay = Math.floor(gap/day);
+        textHour = Math.floor(gap%day/hour);
+        textMin = Math.floor(gap%hour/minute);
+        textSec = Math.floor(gap%minute/second);
+        console.log(countDate);
+        console.log(textDay,textHour,textMin,textSec);
+    }
+
 
     //setting to inner text
     document.getElementById('day').innerText = textDay;
